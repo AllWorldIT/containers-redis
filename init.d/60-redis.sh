50d83d8b7832f38df5f11d604779869bb864ca5d
Nigel Kukard <nkukard@lbsd.net>
2022-12-21 04:05:44 +0000

    Fixed permissions on redis data directory

--- init.d/60-redis.sh
+++ init.d/60-redis.sh
@@ -3,7 +3,7 @@
 chown -R redis:redis /var/lib/redis
 
 chown root:redis /var/lib/redis
-chmod 0750 /var/lib/redis
+chmod 0770 /var/lib/redis
 
 find /var/lib/redis -type f -print0 | xargs -0 --no-run-if-empty chmod 0640
 
