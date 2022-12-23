7d284cbabc9af165d7fe39decc9c6e1249d471d3
Nigel Kukard <nkukard@lbsd.net>
2022-12-23 11:26:23 +0000

    Don't chmod everything, this can take a very long time

--- init.d/60-redis.sh
+++ init.d/60-redis.sh
@@ -5,8 +5,6 @@ chown -R redis:redis /var/lib/redis
 chown root:redis /var/lib/redis
 chmod 0770 /var/lib/redis
 
-find /var/lib/redis -type f -print0 | xargs -0 --no-run-if-empty chmod 0640
-
 echo "NOTICE: Initializing settings"
 
 # Setup the password
